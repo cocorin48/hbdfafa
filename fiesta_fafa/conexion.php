2bea3404b62fd95da73c7454ebdf459c5f0c632d
cocorin48 <ramirekenia7@gmail.com>
2026-07-28 23:33:16 -0600

    Update conexion.php

--- fiesta_fafa/conexion.php
+++ fiesta_fafa/conexion.php
@@ -2,9 +2,10 @@
 $host = "sql102.infinityfree.com";
 $usuario = "if0_42525418";
 $password = "Packof7_XG";
-$bd = "felicitaciones";
+$bd = "if0_42525418_nom";
 $conn = new mysqli($host, $usuario, $password, $bd);
-if($conn->connect_error){
+if($conn->connect_er
+   ror){
     die("Error de conexión: " . $conn->connect_error);
 }
 $conn->set_charset("utf8");
